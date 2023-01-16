0185aa6277ebf484e7a757f8ac2c96879d8124d2
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-01-16 21:19:13 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return (
+      <>
+        <div>
+          <Toaster />
+        </div>
+        <div>HELLO</div>
+      </>
+    );
+  }
+}
